4e47259673bac4570383af7a94a7d21462e6e88e
Ramon Portela <portela.ramon96@gmail.com>
2017-10-05 06:54:46 -0300

    openshift v3 ip adress

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ var io = require('socket.io')(server);
 var fs = require('fs');
 var path = require('path');
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_ip_address = '0.0.0.0' || '127.0.0.1';
 
 app.use(express.static("client"));
 
