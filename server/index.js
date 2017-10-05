7fafc87a1268ae56525c52dc2f448c2d795aa7df
Ramon Portela <portela.ramon96@gmail.com>
2017-10-04 23:36:42 -0300

    Enviroment variables

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-server.listen(80);
+server.listen(server_port, server_ip_address);
 
 io.on('connection', function(socket){
     socket.on('draw', function(data){
