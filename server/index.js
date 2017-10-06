761042dca61d9392a220be4abadc14705df72997
Ramon Portela <portela.ramon96@gmail.com>
2017-10-05 23:45:46 -0300

    serving index page

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var fs = require('fs');
 var path = require('path');
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_port = process.env.PORT || 8080;
 var server_ip_address = '0.0.0.0' || '127.0.0.1';
 
-app.use(express.static("client"));
+app.use('/', express.static("client"));
 
 server.listen(server_port, server_ip_address, function(){
     console.log("Listening on " + server_ip_address + ", server_port " + server_port)
