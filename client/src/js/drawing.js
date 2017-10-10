70c12ace12f2ae90fd8fbaa9e37b765cd3103cbf
Ramon Portela <portela.ramon96@gmail.com>
2017-10-10 00:30:53 -0300

    started css, new font icons, and bad img-header

--- client/src/js/drawing.js
+++ client/src/js/drawing.js
@@ -2,8 +2,17 @@ var canvas = document.getElementById("drawing-canvas");
 var context = canvas.getContext("2d");
 var colorInput = document.getElementById("color-input");
 var btnLimpar = document.getElementById("btn-limpar");
-var btnPincel = document.getElementById("pincel");
-var btnBorracha = document.getElementById("borracha");
+var btnPincel = document.getElementById("btn-pincel");
+var btnBorracha = document.getElementById("btn-borracha");
+var btnBucket = document.getElementById("btn-bucket");
+var btnColorPicker = document.getElementById("btn-color-picker");
+var colorDiv = document.getElementById("color-div");
+
+window.onload = function(e){
+    colorInput.remove();
+}
+
+colorDiv.style.backgroundColor = colorInput.value;
 
 var mouseLocal = {
     click: false,
@@ -18,8 +27,25 @@ var mousesOnline = [];
 var inicioClick = 0;
 
 var line = {
-    color: "black",
+    color: colorInput.value,
     size: 5
+}
+
+function getCurrentColorRGBA(){
+    var c;
+    if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(line.color)){
+        c= line.color.substring(1).split('');
+        if(c.length== 3){
+            c= [c[0], c[0], c[1], c[1], c[2], c[2]];
+        }
+        c= '0x'+c.join('');
+        return {
+            r: (c>>16)&255,
+            g: (c>>8)&255,
+            b: c&255
+        }
+    }
+    throw new Error('Bad Hex');
 }
 
 var draw = function(firstClick){
@@ -43,6 +69,10 @@ var draw = function(firstClick){
             }
             context.lineTo(mouseLocal.currentX, mouseLocal.currentY);
             context.stroke();
+            break;
+        case "bucket":
+            console.log("bucket");
+            fill(line.color);
             break;
     }
 }
@@ -69,22 +99,28 @@ var setLine = function(newSetup){
     }
 }
 
-
-
 colorInput.onchange = function(e){
     line.color = colorInput.value;
+    colorDiv.style.backgroundColor = colorInput.value;
 }
-
 btnLimpar.onclick = function(e){
     socket.emit("clear");
 }
-
 btnBorracha.onclick = function(e){
     mouseLocal.pincel = "borracha";
 }
-
 btnPincel.onclick = function(e){
     mouseLocal.pincel = "pincel";
+}
+// btnBucket.onclick = function(e){
+//     mouseLocal.pincel = "bucket";
+// }
+btnColorPicker.onclick = function(e){
+    mouseLocal.pincel = "picker";
+}
+
+colorDiv.onclick = function(e){
+    colorInput.click();
 }
 
 canvas.onmousedown = function(e){
@@ -95,14 +131,7 @@ canvas.onmousedown = function(e){
     setMouse(e.clientX, e.clientY);
     draw(true);
     pathArray.push({x: mouseLocal.currentX, y: mouseLocal.currentY});
-    switch(mouseLocal.pincel){
-        case "pincel":
-            emitDraw();
-            break;
-        case "borracha":
-            emitBorracha();
-            break;
-    }
+    emitAction();
     mouseLocal.click = true;
 };
 
@@ -112,14 +141,7 @@ canvas.onmousemove = function(e){
         draw(false);
         pathArray.push({x: mouseLocal.currentX, y: mouseLocal.currentY});
         if(pathArray.length == 100){
-            switch(mouseLocal.pincel){
-                case "pincel":
-                    emitDraw();
-                    break;
-                case "borracha":
-                    emitBorracha();
-                    break;
-            }
+            emitAction();
             pathArray = pathArray.slice(99);
         }
     }
@@ -127,6 +149,104 @@ canvas.onmousemove = function(e){
 
 canvas.onmouseup = function(e){
     mouseLocal.click = false;
+    emitAction();
+    pathArray = [];
+}
+
+canvas.onmouseout = function(e){
+    mouseLocal.click = false;
+    if(pathArray.length > 0){
+        emitAction();
+        pathArray = [];
+    }
+}
+
+function fill(newColor){
+    var canvasWidth = canvas.width;
+    var canvasHeight = canvas.height;
+    var imageData = colorLayer = context.getImageData(0,0,canvasWidth,canvasHeight);
+
+
+    pixelStack = [[mouseLocal.currentX, mouseLocal.currentY]];
+    
+    while(pixelStack.length)
+    {
+      var newPos, x, y, pixelPos, reachLeft, reachRight;
+      newPos = pixelStack.pop();
+      x = newPos[0];
+      y = newPos[1];
+      
+      pixelPos = (y*canvasWidth + x) * 4;
+      while(y-- >= 0 && matchStartColor(pixelPos))
+      {
+        pixelPos -= canvasWidth * 4;
+      }
+      pixelPos += canvasWidth * 4;
+      ++y;
+      reachLeft = false;
+      reachRight = false;
+      while(y++ < canvasHeight-1 && matchStartColor(pixelPos))
+      {
+        colorPixel(pixelPos);
+    
+        if(x > 0)
+        {
+          if(matchStartColor(pixelPos - 4))
+          {
+            if(!reachLeft){
+              pixelStack.push([x - 1, y]);
+              reachLeft = true;
+            }
+          }
+          else if(reachLeft)
+          {
+            reachLeft = false;
+          }
+        }
+        
+        if(x < canvasWidth-1)
+        {
+          if(matchStartColor(pixelPos + 4))
+          {
+            if(!reachRight)
+            {
+              pixelStack.push([x + 1, y]);
+              reachRight = true;
+            }
+          }
+          else if(reachRight)
+          {
+            reachRight = false;
+          }
+        }
+                
+        pixelPos += canvasWidth * 4;
+      }
+    }
+    context.putImageData(colorLayer, 0, 0);
+      
+    function matchStartColor(pixelPos)
+    {
+        var rgb = getCurrentColorRGBA();
+      var r = colorLayer.data[pixelPos];	
+      var g = colorLayer.data[pixelPos+1];	
+      var b = colorLayer.data[pixelPos+2];
+    
+      return (r == rgb.r && g == rgb.g && b == rgb.b);
+    }
+    
+    function colorPixel(pixelPos)
+    {
+        var rgb = getCurrentColorRGBA();
+      colorLayer.data[pixelPos] = rgb.r;
+      colorLayer.data[pixelPos+1] = rgb.g;
+      colorLayer.data[pixelPos+2] = rgb.b;
+      colorLayer.data[pixelPos+3] = 255;
+    }
+
+}
+
+function emitAction(){
     switch(mouseLocal.pincel){
         case "pincel":
             emitDraw();
@@ -134,21 +254,11 @@ canvas.onmouseup = function(e){
         case "borracha":
             emitBorracha();
             break;
-    }
-    pathArray = [];
-}
-
-canvas.onmouseout = function(e){
-    mouseLocal.click = false;
-    if(pathArray.length > 0){
-        switch(mouseLocal.pincel){
-            case "pincel":
-                emitDraw();
-                break;
-            case "borracha":
-                emitBorracha();
-                break;
-        }
-        pathArray = [];
+        case "bucket":
+            //to do
+            break;
+        case "picker":
+            //to do
+            break;
     }
 }
