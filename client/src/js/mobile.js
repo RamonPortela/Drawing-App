4a5b21aa204de84096f504809b9fe8b7abb3e18d
Ramon Portela <portela.ramon96@gmail.com>
2017-10-23 02:10:15 -0200

    Fix bucket mobile

--- client/src/js/mobile.js
+++ client/src/js/mobile.js
@@ -4,9 +4,15 @@ canvas.addEventListener("touchcancel", handleCancel, false);
 canvas.addEventListener("touchleave", handleEnd, false);
 canvas.addEventListener("touchmove", handleMove, false);
 
+var lastTouch = {}
+
 function handleStart(e) {
     e.preventDefault();
     var touch = e.touches[0];
+    lastTouch = {
+        clientX: touch.clientX,
+        clientY: touch.clientY
+    };
     var mouseEvent = new MouseEvent("mousedown", {
         clientX: touch.clientX,
         clientY: touch.clientY
@@ -15,17 +21,30 @@ function handleStart(e) {
 }
 function handleMove(e){
     var touch = e.touches[0];
+    lastTouch = {
+        clientX: touch.clientX,
+        clientY: touch.clientY
+    };
     var mouseEvent = new MouseEvent("mousemove", {
       clientX: touch.clientX,
       clientY: touch.clientY
     });
     canvas.dispatchEvent(mouseEvent);
 }
-function handleCancel(e){
-    var mouseEvent = new MouseEvent("mouseup");
+function handleCancel(e){    
+    var touch = e.touches[0];
+    var mouseEvent = new MouseEvent("mouseup", {
+        clientX: touch.clientX,
+        clientY: touch.clientY
+      });
     canvas.dispatchEvent(mouseEvent);
 }
 function handleEnd(e){
-    var mouseEvent = new MouseEvent("mouseup");
+    console.log(e);
+    var touch = e.touches[0];
+    var mouseEvent = new MouseEvent("mouseup", {
+        clientX: lastTouch.clientX,
+        clientY: lastTouch.clientY
+      });
     canvas.dispatchEvent(mouseEvent);
 }
